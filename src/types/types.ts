a8114bf263aafd33d21f1d23223772093f1b40f4
hallvardvo <hallvardvo@gmail.com>
2025-01-29 21:18:15 +0100

    fix: trying to deploy on vercel... smh

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,8 @@
 export interface Track {
   name: string;
   artists: string;
-  image: string;
-  link: string;
+  image?: string;
+  link?: string;
   song_duration: number;
   played_full_song: number;
   play_count: number;
